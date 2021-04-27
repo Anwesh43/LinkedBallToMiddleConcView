0ac2c47d4fe10afea0a65d113fa044e0f4135e6c
Anwesh43 <anweshthecool0@gmail.com>
2021-04-28 03:28:52 +0530

    adding Renderer class

--- balltomiddleconcview/src/main/java/com/example/balltomiddleconcview/BallToMiddleConcView.kt
+++ balltomiddleconcview/src/main/java/com/example/balltomiddleconcview/BallToMiddleConcView.kt
@@ -186,4 +186,27 @@ class BallToMiddleConcView(ctx : Context) : View(ctx) {
             curr.startUpdating(cb)
         }
     }
+
+    data class Renderer(var view : BallToMiddleConcView) {
+
+        private val animator : Animator = Animator(view)
+        private val btmc : BallToMiddleConc = BallToMiddleConc(0)
+        private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+        fun render(canvas : Canvas) {
+            canvas.drawColor(backColor)
+            btmc.draw(canvas, paint)
+            animator.animate {
+                btmc.update {
+                    animator.stop()
+                }
+            }
+        }
+
+        fun handleTap() {
+            btmc.startUpdating {
+                animator.stop()
+            }
+        }
+    }
 }
